9a82866d6f89aa7797ca1497e9f4a057127d4baa
Azim Chowdhury <azimchy994@gmail.com>
2024-03-06 13:12:42 +0600

    style

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>my first android app!</Text>
+      <View style={[styles.box, styles.box1]} />
+      <View style={[styles.box, styles.box2]} />
+      <View style={[styles.box, styles.box3]} />
       <StatusBar style="auto" />
     </View>
   );
@@ -12,13 +14,25 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
+    height: '100%',
+    width: '100%',
+    backgroundColor: 'lightblue',
+    display: 'flex',
     justifyContent: 'center',
-
+    alignItems: 'center',
   },
-  text: {
-    backgroundColor: 'green',
-    fontSize: 40
-  }
+  box: {
+    height: 150,
+    width: 150,
+    marginBottom: 20,
+  },
+  box1: {
+    backgroundColor: 'blue'
+  },
+  box2: {
+    backgroundColor: 'green'
+  },
+  box3: {
+    backgroundColor: 'red'
+  },
 });
