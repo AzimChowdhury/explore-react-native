1cf156af7d1f684272d3e441e6fdad1839d3d890
Azim Chowdhury <azimchy994@gmail.com>
2024-03-10 00:02:32 +0600

    done

--- App.js
+++ App.js
@@ -1,37 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
 import { Text, SafeAreaView, ScrollView, Image, StyleSheet, View } from 'react-native';
 
 export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
+      <View style={styles.profile}>
         <View style={styles.profileHeader}>
           <Image source={{ uri: 'https://github.com/octocat.png' }} style={styles.profileImage} />
           <View>
-            <Text style={styles.usernameText}>The Octocat</Text>
-            <Text style={styles.usernameText}>octocat</Text>
-            <Text style={styles.usernameText}>joined 25 Jan 2011 </Text>
+            <Text style={styles.name}>The Octocat</Text>
+            <Text style={styles.username}>@octocat</Text>
+            <Text style={styles.join}>joined 25 Jan 2011 </Text>
           </View>
         </View>
-        <Text style={styles.bioText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Text>
+        <Text style={styles.bio}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.  </Text>
         <View style={styles.statsContainer}>
-          <View style={styles.statItem}>
-            <Text style={styles.statLabel}>Repos</Text>
-            <Text style={styles.statValue}>8</Text>
+          <View style={styles.stat}>
+            <Text style={styles.label}>Repos</Text>
+            <Text style={styles.value}>8</Text>
           </View>
-          <View style={styles.statItem}>
-            <Text style={styles.statLabel}>Followers</Text>
-            <Text style={styles.statValue}>5454054</Text>
+          <View style={styles.stat}>
+            <Text style={styles.label}>Followers</Text>
+            <Text style={styles.value}>35451</Text>
           </View>
-          <View style={styles.statItem}>
-            <Text style={styles.statLabel}>Following</Text>
-            <Text style={styles.statValue}>9</Text>
+          <View style={styles.stat}>
+            <Text style={styles.label}>Following</Text>
+            <Text style={styles.value}>9</Text>
           </View>
         </View>
-        <Text style={styles.locationText}>Location: San Fransisco</Text>
-        <Text style={styles.linkText}>Blog: b;ph</Text>
-      </ScrollView>
+        <Text style={styles.links}>San Francisco</Text>
+        <Text style={styles.links}>https://github.blog</Text>
+        <Text style={styles.disabledLinks}>Not Available</Text>
+        <Text style={styles.links}>@github</Text>
+      </View>
     </SafeAreaView>
   );
 }
@@ -41,9 +42,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'flex-start',
-    padding: 30,
+    paddingTop: 40,
+    backgroundColor: 'lightgray',
+    padding: 20
+  },
+  profile: {
+    backgroundColor: 'white',
+    height: '100%',
+    borderRadius: 20,
+    paddingVertical: 30,
+    paddingHorizontal: 20
   },
   profileHeader: {
     flexDirection: 'row',
@@ -51,47 +60,65 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   profileImage: {
-    width: 80,
-    height: 80,
+    width: 100,
+    height: 100,
     borderRadius: 50,
-    marginRight: 10,
+    marginRight: 15,
   },
-  usernameText: {
-    fontSize: 18,
+  name: {
+    fontSize: 24,
     fontWeight: 'bold',
   },
-  bioText: {
-    fontSize: 14,
-    color: '#666',
-    marginBottom: 10,
+  username: {
+    color: '#106AFE',
+    paddingVertical: 2,
+    fontSize: 16
+  },
+  join: {
+    color: 'gray',
+    fontSize: 16,
+
+  },
+  bio: {
+    fontSize: 18,
+    color: 'gray',
+    marginVertical: 10,
+    lineHeight: 28,
+    letterSpacing: 1
+
   },
   statsContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     width: '100%',
-    backgroundColor: 'rgba(0, 0, 0,0.2)',
-    padding: 20
+    backgroundColor: 'rgba(0, 0, 0,0.1)',
+    padding: 30,
+    marginVertical: 20,
+    borderRadius: 10
   },
-  statItem: {
+  stat: {
     alignItems: 'center',
   },
-  statValue: {
-    fontSize: 16,
+  value: {
+    fontSize: 18,
     fontWeight: 'bold',
-    marginBottom: 5,
+    marginTop: 5,
   },
-  statLabel: {
-    fontSize: 12,
-    color: '#999',
+  label: {
+    fontSize: 16,
+    color: 'gray',
+    fontWeight: 'semibold'
   },
-  locationText: {
+  links: {
     marginTop: 15,
-    fontSize: 14,
-    color: '#666',
+    fontSize: 18,
+    color: 'gray',
+    letterSpacing: 1
   },
-  linkText: {
-    marginTop: 10,
-    fontSize: 14,
-    color: '#333',
-  },
+  disabledLinks: {
+    marginTop: 15,
+    fontSize: 18,
+    color: 'lightgray',
+    letterSpacing: 1
+  }
 });
